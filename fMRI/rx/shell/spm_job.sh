9ae4a2c90c1f0d0c2bc08a1367d6c15dff614711
Dani Cosme <dani.cosme@gmail.com>
2018-09-16 13:13:45 -0700

    moved scripts

--- fMRI/rx/shell/spm_job.sh
+++ fMRI/rx/shell/spm_job.sh
@@ -1,57 +1,24 @@
 #!/bin/bash
-#--------------------------------------------------------------
-# Inputs:
-#	* SUB = defined in subject_list.txt
-#	* SCRIPT = MATLAB script to create and execute batch job
-#	* Edit SPM path
-#
-# Outputs:
-#	* Creates a batch job for $SUB
-#	* Batch jobs are saved to the path defined in MATLAB script
-#	* Executes batch job
-#
-# D.Cos 2017.3.7
-#--------------------------------------------------------------
+#SBATCH --time=0-00:01:00
+#SBATCH --partition=short
 
-if [[ -z $1 || -z $2 || -z $3 ]]; then
-	if [[ -z $REPLACESID || -z $SCRIPT || -z $SUB ]]; then
-		echo "Aguments not supplied on command line or in environment"
-		exit 1
-	fi
-else
-	# define subject id to replace in script from input 1
-	REPLACESID=$1
+# Set your study
+STUDY=/projects/sanlab/shared/PROP/PROP_scripts
 
-	# define matlab script to run from input 2
-	SCRIPT=$2
+# SPM Path
+SPM_PATH=/projects/sanlab/shared/spm12
 
-	# define subject ID from input 3
-	SUB=$3
-fi
+# Set MATLAB script path
+SCRIPT=${STUDY}/fMRI/rx/models/pst_relevance.m
 
-# MATLAB version
-MATLABVER=R2015b
-SINGLECOREMATLAB=true
-ADDITIONALOPTIONS=""
+# PROP the results files
+RESULTS_INFIX=rx_pst_relevance
 
-if "$SINGLECOREMATLAB"; then
-	ADDITIONALOPTIONS="-singleCompThread"
-fi
+# Set output dir
+OUTPUTDIR=${STUDY}/fMRI/rx/shell/output
 
 
-if [ "${PROCESS}" == "slurm" ]; then
-	module load matlab
-	MATLABCOMMAND=matlab
-else
-	MATLABCOMMAND="/Applications/MATLAB_"${MATLABVER}".app/bin/matlab"
-fi
-
-# create and execute job
-echo -------------------------------------------------------------------------------
-echo "${SUB}"
-echo "Running ${SCRIPT}"
-echo -------------------------------------------------------------------------------
-
-$MATLABCOMMAND -nosplash -nodisplay -nodesktop ${ADDITIONALOPTIONS} -r "clear; addpath('$SPM_PATH'); spm_jobman('initcfg'); sub='$SUB'; script_file='$SCRIPT'; replacesid='$REPLACESID'; run('make_sid_matlabbatch.m'); spm_jobman('run',matlabbatch); exit"
-
-# removed these options due to error warnings: -noFigureWindows
+# run script
+module load matlab
+srun --job-name="${RESULTS_INFIX}" -o "${OUTPUTDIR}"/"${RESULTS_INFIX}".log \
+	 matlab -nosplash -nodisplay -nodesktop -r "clear; addpath('$SPM_PATH'); spm_jobman('initcfg'); spm_jobman('run',$SCRIPT); exit"
