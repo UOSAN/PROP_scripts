66d6adc24a73ab4761cd0e52437cb0103fccb87a
Dani Cosme <dani.cosme@gmail.com>
2019-07-23 10:45:03 +0200

    updated batch scripts

--- fMRI/fx/models/batch_fx_pmod.sh
+++ fMRI/fx/models/batch_fx_pmod.sh
@@ -1,75 +1,53 @@
 #!/bin/bash
 #--------------------------------------------------------------
-# Inputs:
-#	* STUDY = study name
-#	* SUBJLIST = subject_list.txt
-#	* SCRIPT = MATLAB script to create and execute batch job
-#	* PROCESS = running locally, via qsub, or on the Mac Pro
-#	* Edit output and error paths
-#
-# Outputs:
-#	* Executes spm_job.sh for $SUB and $SCRIPT
-#
-# D.Cos 2017.3.7
+# This script executes $SHELL_SCRIPT for $SUB and matlab $SCRIPT
+#	
+# D.Cos 2018.11.06
 #--------------------------------------------------------------
 
-
-# Set your study
-STUDY=/projects/sanlab/shared/PROP/PROP_scripts
+## Set your study
+STUDY=DEV
 
 # Set subject list
 SUBJLIST=`cat subject_list_test.txt`
 
 # Which SID should be replaced?
-REPLACESID='001'
+REPLACESID=001
 
 # SPM Path
 SPM_PATH=/projects/sanlab/shared/spm12
 
-# Set MATLAB script path
-SCRIPT=${STUDY}/fMRI/fx/models/fx_pmod.m
+# Set scripts directory path
+SCRIPTS_DIR=/projects/sanlab/shared/${STUDY}/${STUDY}_scripts
 
-# PROP the results files
+# Set MATLAB script path
+SCRIPT=${SCRIPTS_DIR}/fMRI/fx/models/fx_pmod.m
+
+# Set shell script to execute
+SHELL_SCRIPT=spm_job.sh
+
+# RRV the results files
 RESULTS_INFIX=fx_pmod
 
-# Set output dir
-OUTPUTDIR=${STUDY}/fMRI/fx/shell/schedule_spm_jobs/output
+# Set output dir and make it if it doesn't exist
+OUTPUTDIR=${SCRIPTS_DIR}/fMRI/fx/models/output
 
-# Set processor
-# use "qsub" for HPC
-# use "local" for local machine
-# use "parlocal" for local parallel processing
+if [ ! -d ${OUTPUTDIR} ]; then
+	mkdir -p ${OUTPUTDIR}
+fi
 
-PROCESS=slurm
-
-# Max jobs only matters for par local
-MAXJOBS=8
-
-#Only matters for slurm
+# Set job parameters
 cpuspertask=1
 mempercpu=8G
 
 # Create and execute batch job
-if [ "${PROCESS}" == "slurm" ]; then 
-	for SUB in $SUBJLIST
-	do
-	 echo "submitting via qsub"
-	 sbatch --export=REPLACESID=$REPLACESID,SCRIPT=$SCRIPT,SUB=$SUB,SPM_PATH=$SPM_PATH,PROCESS=$PROCESS  \
-		 --job-name=${RESULTS_INFIX} \
-		 -o "${OUTPUTDIR}"/"${SUB}"_${RESULTS_INFIX}.log \
-		 --cpus-per-task=${cpuspertask} \
-		 --mem-per-cpu=${mempercpu} \
-		 spm_job.sh
-	 sleep .25
-	done
-
-elif [ "${PROCESS}" == "local" ]; then 
-	for SUB in $SUBJLIST
-	do
-	 echo "submitting locally"
-	 bash spm_job.sh ${REPLACESID} ${SCRIPT} ${SUB} > "${OUTPUTDIR}"/"${SUBJ}"_${RESULTS_INFIX}_output.txt 2> /"${OUTPUTDIR}"/"${SUBJ}"_${RESULTS_INFIX}_error.txt
-	done
-
-elif [ "${PROCESS}" == "parlocal" ]; then 
-	parallel --verbose --results "${OUTPUTDIR}"/{}_${RESULTS_INFIX}_output -j${MAXJOBS} bash spm_job.sh ${REPLACESID} ${SCRIPT} :::: subject_list.txt
-fi
+for SUB in $SUBJLIST; do
+ 	sbatch --export ALL,REPLACESID=$REPLACESID,SCRIPT=$SCRIPT,SUB=$SUB,SPM_PATH=$SPM_PATH,  \
+	 	--job-name=${RESULTS_INFIX} \
+	 	-o ${OUTPUTDIR}/${SUB}_${RESULTS_INFIX}.log \
+	 	--cpus-per-task=${cpuspertask} \
+	 	--mem-per-cpu=${mempercpu} \
+	 	--account=sanlab \
+	 	${SHELL_SCRIPT}
+ 	sleep .25
+done
